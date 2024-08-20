7d31260c8c5ae4616f7dfc4d54e364d5cc3a4e1a
Bruno <brunotozidamiao@hotmail.com>
2024-08-20 16:17:48 -0300

    atualizando
    upando o restante dos módulos e o projeto de previsao de renda

---
+++ Módulo 15 - Streamlit/pythonProject/stream-lit.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache_data
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache_data)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+
+st.bar_chart(hist_values)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.map(filtered_data)
+
+
+@st.cache_data  # 👈 Add the caching decorator
+def load_data(url):
+    df = pd.read_csv(url)
+    return df
+
+df = load_data("https://github.com/plotly/datasets/raw/master/uber-rides-data1.csv")
+st.dataframe(df)
+
+st.button("Rerun")
+
+
+
+@st.cache_data
+def add(arr1, arr2):
+	return arr1 + arr2
+
+
+@st.cache_data
+def query():
+    return pd.read_sql_query("SELECT * from table", connection)
+
+@st.cache_data
+def api_call():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts/1')
+    return response.json()
+
+@st.cache_data
+def run_model(inputs):
+    return model(inputs)
+
+col1, col2 = st.columns(2)
+col1.write('Column 1')
+col2.write('Column 2')
+
+# Three columns with different widths
+col1, col2, col3 = st.columns([3,1,1])
+# col1 is wider
+
+# Using 'with' notation:
+with col1:
+    st.write('This is column 1')
+
+# Insert a chat message container.
+with st.chat_message("user"):
+    st.write("Hello 👋")
+    st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget.
+st.chat_input("Say something")
